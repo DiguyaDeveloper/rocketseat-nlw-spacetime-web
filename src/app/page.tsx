f89df2438e354f4df10de58cbd872b79dfc3896b
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-16 23:30:29 -0300

    feat: :sparkles: Adicionando efeito de blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main className="grid min-h-screen grid-cols-2 ">
+      {/** Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      </div>
+
+      {/** Left */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
