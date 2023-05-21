f4fbff628f97d5967e724c618cc0971cf450216e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-21 11:08:03 -0300

    feat: :sparkles: Abstraido elementos layout para o root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamJuree,
 } from 'next/font/google'
+import { Blur } from '@/components/blur/Blur'
+import { Stripes } from '@/components/stripes/Stripes'
+import { Profile } from '@/components/profile/Profile'
+import { SignIn } from '@/components/signin/Signin'
+import { Hero } from '@/components/hero/Hero'
+import { Copyright } from '@/components/copyright/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamJuree = BaiJamJuree({
@@ -19,11 +26,27 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamJuree.variable} bg-gray-900 font-sans text-gray-100`}>
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/** Left Container */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            <Blur />
+            <Stripes />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/** Right Container */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,5 @@
-import { cookies } from 'next/headers'
-import { Blur } from '@/components/blur/Blur'
-import { Copyright } from '@/components/copyright/Copyright'
 import { EmptyMemories } from '@/components/empty-memories/EmptyMemories'
-import { Hero } from '@/components/hero/Hero'
-import { SignIn } from '@/components/signin/Signin'
-import { Stripes } from '@/components/stripes/Stripes'
-import { Profile } from '@/components/profile/Profile'
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/** Left Container */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        <Blur />
-        <Stripes />
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      {/** Right Container */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
