a015a9b36454b145eaa75d805345445710274c6c
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-05-21 22:34:02 -0300

    feat: :sparkles: Adicionado padding

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function NewMemory() {
   return (
-    <div className="flex1 flex flex-col gap-4">
+    <div className="flex1 flex flex-col gap-4 p-16">
       <Link
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100">
